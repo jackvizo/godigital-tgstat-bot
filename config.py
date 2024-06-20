4df3027bd35f83b8553929fbde25f2ad904ca0ec
Jack <j@m.ru>
2024-06-20 18:46:03 +0700

    Initial migrations on database

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ db_name = "tgbot_db"
 # SQLAlchemy
 dialect = "postgresql"
 driver = "psycopg"              # psycopg => 'psycopg3'
+
 # dialect+driver://username:password@host:port/database
+# postgresql+psycopg://tgbot:pass_123@localhost/tgbot_db
 db_engine = f"{dialect}+{driver}://{user}:{password}@{host}:{port}/{db_name}"
 
 # telegram
