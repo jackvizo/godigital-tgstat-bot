d4f20683009cda94f3a8b530a8396ec94a27d79c
Jack <j@m.ru>
2024-06-26 02:20:44 +0700

    Add legacy to test.py

--- test.py
+++ test.py
@@ -1,6 +1,46 @@
-import types
-from datetime import timedelta, datetime, time
-from models import Stat_reaction, Stat_post
+import asyncio
+from datetime import timedelta, datetime
+from time import time
+
+from prefect.blocks.system import String
+from telethon import functions, types, events
+
+from backend.asyncSQLDataService import asyncSQLDataService
+from models import Stat_reaction, Stat_post, Stat_user
+
+
+dbname = String.load("db").value
+groups = [gr.strip() for gr in String.load("groups").value.split(',')]
+
+log = None
+
+async def run_logging(client, max_posts=15, parent=''):
+    global log
+    log = []
+    print('log: ', log)
+    log.append(parent+' start: ' + datetime.now().strftime("%H:%M:%S"))
+    for channel_name in groups:
+        print('run: ', channel_name)
+        res = asyncio.create_task(get_posts(client, await read_channels(client, channel_name),
+                                            max_posts=max_posts, parent=parent))
+        # res = await get_posts(client, await read_channels(client, channel_name), max_posts=max_posts, parent=parent)
+        pass
+        if not res:
+            log.append(['error: ', channel_name, datetime.now().strftime("%H:%M:%S")])
+        else:
+            pass
+            # log.append([res[0], res[1], datetime.now().strftime("%H:%M:%S")])
+    # log.append(' end: '+datetime.now().strftime("%H:%M:%S"))
+    log.append(' start2pgsql: ' + datetime.now().strftime("%H:%M:%S"))
+    # if platform == 'win32':
+    #     print("win32 - skip write to mysql")
+    #     await update2mysql()
+    # else:
+    #     await update2mysql()
+    await update2pgsql()
+
+    log.append('  end2pgsql: ' + datetime.now().strftime("%H:%M:%S"))
+    print('log: ', log)
 
 
 async def get_posts(client, id, max_posts=1500, parent=None):
@@ -174,3 +214,83 @@ async def get_comments(client, id, msg_id, max=300, sql=None):
                 pass
                 if counter_comments >= total_comments:
                     return comments_channels_count, comments_users_count, comments_messages_count
+
+
+async def read_channels(client, search_name):
+    try:
+        if type(int(search_name)) == int:
+            pass
+            search_name = str(search_name).replace('-100', '')
+            id = int(search_name)
+            # print(id)
+            try:
+                item = await client.get_entity(id)
+                # print(item)
+                return item.id
+            except Exception as e:
+                print(e)
+                return None
+    except ValueError as e:
+        pass
+        # print(e)
+    dialogs = await client.get_dialogs()
+    for dial in dialogs:
+        item = await client.get_entity(dial.id)
+        try:
+            username = item.username
+        except Exception as e:
+            username = None
+
+        if dial.is_group:
+            print('[group ]'+Back.GREEN+Fore.BLACK, dial.id,  dial.title, username, dial.is_user, dial.is_channel, dial.is_group, Style.RESET_ALL)
+        elif dial.is_channel:
+            print('[chanel]'+Back.RED+Fore.BLACK, dial.id,  dial.title, username, dial.is_user, dial.is_channel, dial.is_group, Style.RESET_ALL)
+        else:
+            print('[others]'+Back.YELLOW+Fore.BLACK, dial.id,  dial.title, username, dial.is_user, dial.is_channel, dial.is_group, Style.RESET_ALL)
+
+        if ((dial.title).lower() == search_name.lower()) or (search_name.lower() in (dial.title).lower()):
+            return dial.id
+    return None
+
+
+async def write2pgsql(matrix: list, base_name: str):
+    try:
+        mysql = MYSQLDataService(hostname=clientAPI.mysql_host, user=clientAPI.mysql_login, password=clientAPI.mysql_pass,
+                                 databaseName=''+str('posts'), forceCreation=False)
+
+    except Exception as e:
+        print(e)
+        return False
+    # mysql = MYSQLDataService(hostname="92.255.111.148", user='user_test3', password='QW12er34!!', databaseName=base_name_to_check, forceCreation=True)
+    print('start write 2 sql ', datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+    res = mysql.store_table(base_name, matrix)
+    if res is None:
+        print('ERROR1 write 2 sql ', datetime.now().strftime("%d.%m.%Y %H:%M:%S"), " wait 5 sec and retry")
+        await asyncio(5)
+        res = mysql.store_table(base_name, matrix)
+        if res is None:
+            print('ERROR2 write 2 sql ', datetime.now().strftime("%d.%m.%Y %H:%M:%S"), " exiting")
+            return False
+
+    print('end write 2 sql ', datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+    return True
+
+
+async def update2pgsql():
+    sql = asyncSQLDataService()
+    await sql.init('posts.db', forceCreation=True)
+    try:
+        res = await sql.get_all_post()
+        res1 = await sql.get_all_react()
+        await sql.close()
+
+        if await write2pgsql(res, 'stat_post'):
+            if await write2pgsql(res1, 'stat_reaction'):
+                print('all OK')
+            else:
+                print('error update table `stat_post`')
+        else:
+            print('error update table `stat_reaction`')
+
+    except Exception as e:
+        print('error update PgSQL: \n', str(e))
