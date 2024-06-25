08d48fe18f2140d18da14331c571b5221cf00f1e
Jack <j@m.ru>
2024-06-25 20:21:04 +0700

    Add planning.py

---
+++ planning.py
@@ -0,0 +1,39 @@
+import httpx
+from datetime import datetime, timedelta
+
+PREFECT_SERVER_URL = 'http://localhost:4200'
+
+
+def get_deployment_by_flow_name(flow_name: str):
+    response = httpx.post(f"{PREFECT_SERVER_URL}/deployments/filter", json={
+        "flows": {
+            "name": {
+                "any_": [flow_name]
+            }
+        }
+    })
+    json = response.json()
+    return json[0] if json else None
+
+
+def create_scheduled_flow_run(deployment_id: str, scheduled_start_time: datetime):
+    response = httpx.post(f"{PREFECT_SERVER_URL}/deployments/{deployment_id}/create_flow_run", json={
+        "parameters": {},
+        "state": {
+            "type": "SCHEDULED",
+            "state_details": {
+                "scheduled_time": scheduled_start_time.isoformat()
+            }
+        }
+    })
+    print(response)
+
+
+deployment = get_deployment_by_flow_name("tg_collect_flow")
+deployment_id = deployment.get('id')
+
+scheduled_start_time_1 = datetime.utcnow() + timedelta(hours=1)
+scheduled_start_time_24 = datetime.utcnow() + timedelta(hours=24)
+
+create_scheduled_flow_run(deployment_id, scheduled_start_time_1)
+create_scheduled_flow_run(deployment_id, scheduled_start_time_24)
