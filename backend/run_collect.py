531017ce789c435b8adb5f43488acce947db6893
Jack <j@m.ru>
2024-07-03 17:23:26 +0700

    Add check to schedule planning

--- backend/run_collect.py
+++ backend/run_collect.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 PREFECT_SERVER_URL = 'http://localhost:4200/api'
 
+# http://localhost:4200/api//deployments/filter
+# http://localhost:4200/api/281c6fb8-e00e-4397-a4c5-f091e621960c/create_flow_run
 
 def get_deployment_by_flow_name(flow_name: str):
     response = httpx.post(f"{PREFECT_SERVER_URL}/deployments/filter", json={
@@ -31,13 +33,19 @@ def create_scheduled_flow_run(deployment_id: str, scheduled_start_time: datetime
 
 def service_run(flow_name="tg_collect", date_start=None):
     deployment = get_deployment_by_flow_name(flow_name)
-    deployment_id = deployment.get('id')
+    try:
+        deployment_id = deployment.get('id')
+    except Exception as e:
+        deployment_id = None
+        print(f'Ошибка получения "id" в ответе сервера: {e}')
+        print(f'Запуск не запланирован!')
 
-    scheduled_start_time_1 = date_start or datetime.utcnow() + timedelta(hours=1)
-    scheduled_start_time_24 = date_start or datetime.utcnow() + timedelta(hours=24)
+    if deployment_id:
+        scheduled_start_time_1 = date_start or datetime.utcnow() + timedelta(hours=1)
+        scheduled_start_time_24 = date_start or datetime.utcnow() + timedelta(hours=24)
 
-    create_scheduled_flow_run(deployment_id, scheduled_start_time_1, hours=1)
-    create_scheduled_flow_run(deployment_id, scheduled_start_time_24, hours=24)
+        create_scheduled_flow_run(deployment_id, scheduled_start_time_1, hours=1)
+        create_scheduled_flow_run(deployment_id, scheduled_start_time_24, hours=24)
 
 #
 # if __name__ == '__main__':
